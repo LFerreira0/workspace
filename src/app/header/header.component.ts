8e23feb59655903b038087415c3a8a731c42b77b
Lucas Ferreira <lucascandido.ferreira@hotmail.com>
2023-06-21 23:59:58 -0300

    First commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  exibirMenu = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public exibirMenuSecundario(){
+    console.log("teste");
+      this.exibirMenu = !this.exibirMenu;
+  }
+
+
+}
