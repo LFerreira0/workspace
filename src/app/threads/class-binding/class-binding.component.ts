1087d90ebeadb21e98bb310396757e112d13326f
Lucas Ferreira <lucascandido.ferreira@hotmail.com>
2023-06-29 20:02:58 -0300

    criação do modulo de binding

--- src/app/threads/class-binding/class-binding.component.ts
+++ src/app/threads/class-binding/class-binding.component.ts
@@ -10,6 +10,10 @@ export class ClassBindingComponent implements OnInit {
   boolean1 = false;
   boolean2 = true;
   urlImagem = 'https://loremflickr.com/100/50?random=2';
+  valorAtual: string = "";
+  valorSalvo: string = "";
+  isMouseOver: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -17,5 +21,21 @@ export class ClassBindingComponent implements OnInit {
 
   public getValor(){
     return 3;
+  }
+
+  public onClick(){
+    alert("Executado")
+  }
+
+  public onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  public onKeyUpEnter(event:any){    
+    this.valorSalvo = event;  
+  } 
+
+  public onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 }
